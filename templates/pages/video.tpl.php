21ec343dca65763c022ff3f3a7a1c75edba69e8b
Balogh Márk <bmark9812@gmail.com>
2024-05-10 20:08:08 +0200

    Add files via upload

--- templates/pages/video.tpl.php
+++ templates/pages/video.tpl.php
@@ -1,7 +1,17 @@
-<iframe width="600" height="300" src="https://www.youtube.com/watch?v=PCOwJC2WuM8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
+<iframe width="600" height="300" src="https://www.youtube.com/embed/PCOwJC2WuM8?si=LGH9BBjmABXCt6ce" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <br>
 <hr>
-
+<iframe 
+src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43127.55890443007!2d19.04221836417235!3d47.
+500190021892585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741dbf3ec6822fb%3A0x64184bf11550bcd7!2zTmVtemV0a8O2emkgT2x0w7Nrw7Z6cG9udA!5e0!3m2!1shu!2shu!4v1715363787146!5m2!1shu!2shu" 
+width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" width="600" height="450" frameborder="0" style="border:0"allowfullscreen ></iframe>
+</iframe>
+<br>
+<a target="_blank" 
+href="https://www.google.com/maps/place/Nemzetk%C3%B6zi+Olt%C3%B3k%C3%B6zpont/@47.50019,19.0422184,13z/data=!4m10!1m2!2m1!1zbmVtemV0a8O2emkgb2x0w7Nrw7Z6cG9udCBoZWx5!3m6!1s0x4741dbf3ec6822fb:0x64184bf11550bcd7!8m2!3d47.523482!4d19.062457!15sCh5uZW16ZXRrw7Z6aSBvbHTDs2vDtnpwb250IGhlbHmSAQ5tZWRpY2FsX2NlbnRlcuABAA!16s%2Fg%2F11bbt6n5q3?entry=ttu">Nagyobb térkép</a>
+<br>
+<br>
 <video width="400" controls>
   <source src="Video.mp4" type="Video/mp4">
   Your browser does not support HTML5 video.
