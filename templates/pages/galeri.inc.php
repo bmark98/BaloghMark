e649ec75e6471c722ff9bd407a8abcfadcc754ac
Balogh Márk <bmark9812@gmail.com>
2024-11-27 23:20:13 +0100

    Delete templates directory

--- templates/pages/galeri.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $MAPPA = 'images/fel/';
-   $TIPUSOK = array ('.jpg', '.png');
-   $MEDIATIPUSOK = array('image/jpeg', 'image/png');
-   $DATUMFORMA = "Y.m.d. H:i";
-   $MAXMERET = 500*1024;
-
-?>
